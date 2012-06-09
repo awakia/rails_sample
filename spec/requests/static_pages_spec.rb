8836934c7ebef383600fa513241aa600f04bd929
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2012-06-10 02:19:35 +0900

    Add about page and its test.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,4 +14,11 @@ describe "Static pages" do
       page.should have_content('Help')
     end
   end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+  end
 end
