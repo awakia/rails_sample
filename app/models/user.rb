6f90ae7f5e4ffb3e0f6a95bcb59896405afea36d
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2012-06-20 19:15:23 +0900

    Add presence validation for name and email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,7 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+
+  validates :name, presence: true
+  validates :email, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,10 +13,24 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when email is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
 end
